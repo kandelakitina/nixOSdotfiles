f228011b63ddc74ac0bfebf285d9d005252ab0a7
Mikhail Eremeneko <eremenko86@gmail.com>
2024-02-09 20:13:03 +0300

    Change as LibrePhoenix syntax

--- flake.nix
+++ flake.nix
@@ -13,32 +13,43 @@
     # hardware.url = "github:nixos/nixos-hardware";
   };
 
-  outputs = { self, nixpkgs, home-manager, ...  } @ inputs: 
+  outputs = { self, nixpkgs, home-manager, ...  }: 
   let
-    inherit (self) outputs;
+    # inherit (self) outputs;
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
       # replace with your hostname
-      nixos = nixpkgs.lib.nixosSystem {
+      nixos = lib.nixosSystem {
         # specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
-        system = "x86_64-linux";
-        modules = [./nixos/configuration.nix];
+        # system = "x86_64-linux";
+        inherit system;
+        modules = [ ./nixos/configuration.nix ];
       };
     };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      # replace with your username@hostname
-      "boticelli@nixos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+      boticelli = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        module = [ ./home-manager/home.nix ];
       };
     };
+    
+    # # Standalone home-manager configuration entrypoint
+    # # Available through 'home-manager --flake .#your-username@your-hostname'
+    # homeConfigurations = {
+    #   # replace with your username@hostname
+    #   "boticelli@nixos" = home-manager.lib.homeManagerConfiguration {
+    #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+    #     extraSpecialArgs = {inherit inputs outputs;};
+    #     # > Our main home-manager configuration file <
+    #     modules = [./home-manager/home.nix];
+    #   };
+    # };
   };
 }
