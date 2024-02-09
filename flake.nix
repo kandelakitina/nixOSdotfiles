84f820cd75585383fed71a1af43c159be83ae1ec
kandelakitina <108508127+kandelakitina@users.noreply.github.com>
2024-02-09 22:40:35 +0300

    fix args

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     nixosConfigurations = {
       # replace with your hostname
       nixos = lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
+        # specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
         # system = "x86_64-linux";
         inherit system;
