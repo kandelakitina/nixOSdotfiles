d2407eea6ea4ff1d49cde2005d40a4eca1500657
kandelakitina <108508127+kandelakitina@users.noreply.github.com>
2024-02-09 22:43:55 +0300

    inputs fix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -137,7 +137,7 @@
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+  # nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
