9fce2af53da2c73dd3cd343ada0ef0df167a7d89
Mikhail Eremeneko <eremenko86@gmail.com>
2024-02-09 18:06:34 +0300

    Minor update

--- flake.nix
+++ flake.nix
@@ -9,12 +9,8 @@
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
+    # Add any other flake you might need
     # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = { self, nixpkgs, home-manager, ...  } @ inputs: 
@@ -24,8 +20,8 @@
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
-      # FIXME replace with your hostname
-      boticelli = nixpkgs.lib.nixosSystem {
+      # replace with your hostname
+      nixos = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
         modules = [./nixos/configuration.nix];
@@ -35,7 +31,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      # FIXME replace with your username@hostname
+      # replace with your username@hostname
       "boticelli@nixos" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,13 +1,8 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{ inputs, lib, config, pkgs, ... }:
+
 {
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
-  # You can import other NixOS modules here
   imports = [
     # If you want to use modules from other flakes (such as nixos-hardware):
     # inputs.hardware.nixosModules.common-cpu-amd
@@ -20,37 +15,115 @@
     ./hardware-configuration.nix
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
+  # bootloader
+  boot.loader = {
+    grub = {
+      enable = true;
+      version = 2;
+      device = "nodev";
+      efiSupport = true;
+    };
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
     };
   };
 
-  # list default apps
+  # networking
+  networking = {
+    networkmanager.enable = true;
+    hostName = "nixos"; # edit this to your liking
+  };
+
+  # QEMU-specific - allow copy buffer VM - host
+  services.spice-vdagentd.enable = true;
+  services.qemuGuest.enable = true;
+
+  # locales
+  # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  time.timeZone = "Europe/Moscow";
+  i18n.defaultLocale = "en_US.UTF-8";
+ 
+  # graphics
+  services.xserver = {
+    enable = true;
+    # setup the resolution to match your screen
+    resolutions = [{ x = 1920; y = 1080; }];
+    virtualScreen = { x = 1920; y = 1080; };
+    layout = "us"; # keyboard layout
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
+    };
+    displayManager.defaultSession = "xfce";
+    autorun = true; # run on graphic interface startup
+    libinput.enable = true; # touchpad support
+  };
+
+  # audio
+  sound.enable = true;
+  nixpkgs.config.pulseaudio = true;
+  hardware.pulseaudio.enable = true;
+
+  # USERS
+  users.users = {
+    # Replace with your username
+    boticelli = {
+      # You can set an initial password for your user.
+      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
+      # Be sure to change it (using passwd) after rebooting!
+      initialPassword = "123";
+      createHome = true;
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        # "..."
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
+      extraGroups = ["wheel"];
+    };
+  };
+
+  # SSH
+  services.openssh.settings = {
+    enable = true;
+    kexAlgorithms = [ "curve25519-sha256" ];
+    ciphers = [ "chacha20-poly1305@openssh.com" ];
+    passwordAuthentication = false;
+    permitRootLogin = "no"; # do not allow to login as root user
+    kbdInteractiveAuthentication = false;
+  };
+
+    # list default apps
   environment.systemPackages = with pkgs; [
-    vim
+    helix
     wget
     git
+    curl
+    btop
   ];
 
   # some default preferences
   environment.variables = {
     EDITOR = "hx"; 
     VISUAL = "hx";
+  };
+
+  nixpkgs = {
+    # Configure your nixpkgs instance
+    config = {
+      allowUnfree = true;
+    };
+
+    # overlays = [
+    #   # If you want to use overlays exported from other flakes:
+    #   # neovim-nightly-overlay.overlays.default
+    #   # Or define it inline, for example:
+    #   # (final: prev: {
+    #   #   hi = final.hello.overrideAttrs (oldAttrs: {
+    #   #     patches = [ ./change-hello-to-hi.patch ];
+    #   #   });
+    #   # })
+    # ];
   };
 
   # This will add each flake input as a registry
@@ -68,48 +141,12 @@
     })
     config.nix.registry;
 
+  # FLAKES SUPPORT
   nix.settings = {
     # Enable flakes and new 'nix' command
     experimental-features = "nix-command flakes";
     # Deduplicate and optimize nix store
     auto-optimise-store = true;
-  };
-
-  # FIXME: Add the rest of your current configuration
-
-  # TODO: Set your hostname
-  networking.hostName = "nixos";
-
-  # TODO: This is just an example, be sure to use whatever bootloader you prefer
-  boot.loader.systemd-boot.enable = true;
-
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  users.users = {
-    # FIXME: Replace with your username
-    boticelli = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "temppassword";
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel"];
-    };
-  };
-
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Use keys only. Remove if you want to SSH using password (not recommended)
-      PasswordAuthentication = false;
-    };
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
